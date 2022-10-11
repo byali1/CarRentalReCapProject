96b4622cf23f028f0516d67d8d1fdf00ae5fb401
Ali Ilgın Kıdır <ali_ilgin@outlook.com>
2022-10-11 09:58:22 +0300

    Added WebAPI

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -21,7 +21,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Brand>> GetAll()
         {
-            if (DateTime.Now.Hour == 20)
+            if (DateTime.Now.Hour == 22)
             {
                return new ErrorDataResult<List<Brand>>(Messages.MaintenanceTime);
 
